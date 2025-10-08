909302eb8caf410c296d23bac743f5d1ea1957dd
nikitapethani <nikitapethani3@gmail.com>
2025-10-08 09:39:51 +0530

    Add files via upload

---
+++ ass_1041_13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int N,i;
+printf("enter the value of n:-");
+scanf("%d",&N);
+printf("the first %d odd numbers are:\n,N";)
+for(i = 1; i<= 2*N; i +=2)
+{
+ printf("%d",i);
+}
+  printf("\n");
+
+return 0;
+}
+ 
