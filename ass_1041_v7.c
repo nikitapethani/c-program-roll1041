4d727cf2c26405632f128e0dfeea72e559d48ecb
nikitapethani <nikitapethani3@gmail.com>
2025-10-08 09:41:55 +0530

    Add files via upload

---
+++ ass_1041_v7.c
@@ -0,0 +1,15 @@
+//write a program calculate the area of a triangle
+/*
+  a=(h*b)/2
+*/
+#include<stdio.h>
+void main()
+{
+  float h,b,a;
+  printf("enter a value:-");
+  scanf("%f",&h);
+  printf("enter a value:-");
+  scanf("%f",&b);
+  a=(h*b)/2;
+  printf("a=%f",a);
+}
