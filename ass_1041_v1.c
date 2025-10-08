f0c3641be33240d69e30e2601ad13f87e7632105
nikitapethani <nikitapethani3@gmail.com>
2025-10-08 09:40:08 +0530

    Add files via upload

---
+++ ass_1041_v1.c
@@ -0,0 +1,11 @@
+// write a program converts kilometers to miles
+#include<stdio.h>
+int main()
+{
+  int kilometers,meters;
+  printf("enter the kilometers value:-");
+  scanf("%d",&kilometers);
+  meters = kilometers * 1000;
+  printf("meters=%d",meters);
+return 0;
+}
