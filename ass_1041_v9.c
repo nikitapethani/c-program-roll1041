1a8f3578172036c1359da8cd55f670f110bd1e6e
nikitapethani <nikitapethani3@gmail.com>
2025-10-08 09:42:21 +0530

    Add files via upload

---
+++ ass_1041_v9.c
@@ -0,0 +1,14 @@
+//write a program calculate the area of the rectangle
+#include<stdio.h>
+int main()
+{ 
+ float lenght,width,area;
+ printf("enter the length of the rectangle:-");
+ scanf("%f",&lenght);
+ printf("enter the width of the rectangle:-");
+ scanf("%f",&width);
+ area = lenght * width;
+ printf("enter the area of the rectangle is:%2f\n",area);
+ scanf("%f",&area);
+return 0;
+}
